3d8e001c0faee6c97df30fb2dec901fd8718e51d
Aleksey <alekseyshaykin2311@gmail.com>
2022-10-04 12:56:36 +0700

    Task #2 has done

--- Seminar_1_Cs/Program.cs
+++ Seminar_1_Cs/Program.cs
@@ -1,5 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
 
 
-// dssdsa
+Console.Write("A = ");
+double value = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("B = ");
+double value2 = Convert.ToDouble(Console.ReadLine());
+
+if (value > value2)
+{
+    Console.WriteLine("-> max = " + value);
+}
+
+else
+{
+    Console.WriteLine("-> max = " + value2);
+}
+
+
+
