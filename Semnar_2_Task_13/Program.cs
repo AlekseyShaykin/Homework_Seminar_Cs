997934181bf07fbfb680d8819c4147da195fb79a
Aleksey <alekseyshaykin2311@gmail.com>
2022-10-06 14:11:44 +0700

    Task #13 has done

--- Semnar_2_Task_13/Program.cs
+++ Semnar_2_Task_13/Program.cs
@@ -1,16 +1,48 @@
 ﻿// Задача 13: Напишите метод, который выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
 // 645 -> 5
-
 // 78 -> третьей цифры нет
-
 // 32679 -> 6
 
-
-string Method(string (n))
+double GetValue()
 {
-Text = Convert.ToString(ReadLine(n));
-return text;
-
-
+    Console.Write("Введите любое число ");
+    double x = Convert.ToDouble(Console.ReadLine());
+    return x;
 }
+
+
+double Lenght(double y)
+{
+
+    double count = 1;
+    while ((y /= 10) > 1)
+        ++count;
+    return count;
+}
+
+double ThirdNumber(double x)
+{
+    double p = 0;
+    double thirdNumb = 0;
+    double stepen = (Lenght(x) - 2);
+    double stepen2 = Math.Pow(10, (Lenght(x) - 3));
+    if (Lenght(x) > 2)
+    {
+        p = x % Math.Pow(10, stepen);
+        thirdNumb = Math.Truncate(p / stepen2);
+    }
+    else
+    {
+        Console.WriteLine("во введенном числе третьей цифры нет");
+    }
+    return thirdNumb;
+  }
+
+
+double N = GetValue();
+double result = ThirdNumber(N);
+
+Console.WriteLine("Третья цифра числа составляет " + result);
+
+
