8dd61191d2d64068539770d88a56966bf45bd0cf
Aleksey <alekseyshaykin2311@gmail.com>
2022-10-07 22:25:02 +0700

    Task #15 has done

---
+++ Seminar_2_Task_15/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 15: Напишите метод, который принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+double GetValue()
+{
+Console.WriteLine("Введите цифру, соответствующую дню недели ");
+
+double x = Convert.ToDouble(Console.ReadLine());
+return x;
+}
+
+
+double Method(double y)
+{
+if ( (y / 5) > 1)
+{Console.WriteLine("--> да");
+}
+else
+{
+Console.WriteLine("--> нет"); 
+}
+ return y;
+}
+
+
+double c = GetValue();
+double x = Method(c);
